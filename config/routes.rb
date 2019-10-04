be194b7ee708b03bb209ef1c2fb2d069f2f117f6
Allison Howard <alliemhow@gmail.com>
2019-10-04 19:26:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/authors/new", to: "authors#new", as: "new_author"
+  post "/authors", to: "authors#create"
+  get "/authors/:id", to: "authors#show", as: "author"
+  get "/authors/:id/edit", to: "authors#edit", as: "edit_author"
+  patch "/authors/:id", to: "authors#update"
 end
